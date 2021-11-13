a8f2ef4a4b7681099d271bc3df72b1587ac118ef
droxy <chiomaotu22@gmail.com>
2021-11-13 12:50:27 +0100

    added bootstrap cdn and bootstrap classes

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
+//Select Menu Elements
 const hamburgerMenu = document.querySelector('.mobile-menu');
 const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('.nav-items a');
 const navItems = document.querySelectorAll('.nav-items');
 
+//Toggle menu open and close
 const ToggleNav = () => {
   hamburgerMenu.classList.toggle('active');
   nav.classList.toggle('active');
@@ -10,7 +12,7 @@ const ToggleNav = () => {
 
 hamburgerMenu.addEventListener('click', ToggleNav);
 
-//
+// Menu closes after when a link is clicked
 const closeMenu = () => {
   hamburgerMenu.classList.remove('active');
   nav.classList.remove('active');
@@ -18,7 +20,7 @@ const closeMenu = () => {
 
 navLinks.forEach((navLink) => navLink.addEventListener('click', closeMenu));
 
-//adding active state to each LI
+//adding active state to lis after being clicked
 navItems.forEach((navItem) =>
   navItem.addEventListener('click', function () {
     let current = document.getElementsByClassName('activeState');
